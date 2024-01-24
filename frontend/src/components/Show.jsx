09648de75ef59df1702ab74cdccb35f5d4f4b4ce
Saptarshi samanta <79819563+yoursaptarshi@users.noreply.github.com>
2024-01-25 00:26:23 +0530

    Update Show.jsx

--- frontend/src/components/Show.jsx
+++ frontend/src/components/Show.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Show = () => {
   const [key,setKey]=useState("");
@@ -9,7 +10,7 @@ const Show = () => {
     const div = document.querySelector(".show_download_button")
     try {
       
- let data = await axios.post("http://15.206.45.43:5000/get",{key:key})
+ let data = await axios.post("http://127.0.0.1:5000/get",{key:key})
  
  setUrl(data.data.message);
       div.style.display="block";
@@ -22,6 +23,8 @@ const Show = () => {
 
     <div className="show_main_container" style={{display:'flex',alignItems:'center',justifyContent:'center',backgroundColor:'grey',minWidth:'100vw',minHeight:'100vh'}}>
     <div className="show_child_container"  style={{minWidth:'75vw',minHeight:'75vh',backgroundColor:'white',display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column'}}>
+    <h1>Samanta Secret Box</h1> 
+     <p>share secret files, by secret keys</p>
     <div className="show_input_container" style={{margin:'2vw'}} >
        <p>Enter your Object id: </p><input type='text' value={key} onChange={(e)=>{setKey(e.target.value)}}/>
       </div>
@@ -32,7 +35,18 @@ const Show = () => {
        <p>Here is your url to download the file, the url will expire in 2 minutes</p>
       <a href={url}>{url}</a> 
       </div>
+      <div className="menu" style={{display:'flex'}}>
+      <div className="menu_upload" style={{margin:'0 2vw'}}>
+        <Link to="/upload">Upload</Link>
+      </div>
+      <div className="menu_show" style={{margin:'0 2vw'}}>
+      <Link to="/show">Show</Link>
+      </div>
+      <div className="menu_delete" style={{margin:'0 2vw'}}>
+      <Link to="/delete">Delete</Link>
+      </div>
     </div>
+      </div>
     </div>
   );
 }
