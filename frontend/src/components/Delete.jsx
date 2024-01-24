ec06400bfe9348d8aad28e0ceba402ffa89a580c
Saptarshi samanta <79819563+yoursaptarshi@users.noreply.github.com>
2024-01-25 00:27:11 +0530

    Update Delete.jsx

--- frontend/src/components/Delete.jsx
+++ frontend/src/components/Delete.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Delete = () => {
   const[key,setKey]=useState("");
@@ -16,8 +17,12 @@ const Delete = () => {
   }
   return (
     <div className="delete_main_container" style={{display:'flex',alignItems:'center',justifyContent:'center',backgroundColor:'grey',minWidth:'100vw',minHeight:'100vh'}}>
+      
       <div className="delete_child_container" style={{minWidth:'75vw',minHeight:'75vh',backgroundColor:'white',display:'flex',alignItems:'center',justifyContent:'center',flexDirection:'column'}}>
+     <h1>Samanta Secret Box</h1> 
+     <p>share secret files, by secret keys</p>
       <div className="delete_input_box" style={{margin:'2vw'}}>
+        <p>Enter your Key:</p>
         <input type="text" value={key} onChange={(e)=>{(setKey(e.target.value))}}></input>
       </div>
       <div className="delete_submit_button" style={{margin:'2vw'}}>
@@ -26,6 +31,17 @@ const Delete = () => {
       <div className="detete_message_box" style={{margin:'2vw'}}>
         <p>{msg}</p>
       </div>
+      <div className="menu" style={{display:'flex'}}>
+      <div className="menu_upload" style={{margin:'0 2vw'}}>
+        <Link to="/upload">Upload</Link>
+      </div>
+      <div className="menu_show" style={{margin:'0 2vw'}}>
+      <Link to="/show">Show</Link>
+      </div>
+      <div className="menu_delete" style={{margin:'0 2vw'}}>
+      <Link to="/delete">Delete</Link>
+      </div>
+    </div>
       </div>
     </div>
   )
